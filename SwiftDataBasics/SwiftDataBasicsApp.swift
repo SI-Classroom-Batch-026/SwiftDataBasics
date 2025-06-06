f27dca9a9870865811c9cdb67759eda690eb911f
MarxDen <dennis.marx@syntax-institut.de>
2025-06-06 13:12:24 +0200

    Initial Commit

---
+++ SwiftDataBasics/SwiftDataBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataBasicsApp.swift
+//  SwiftDataBasics
+//
+//  Created by Dennis on 06.06.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
